b5e5a7bf69f4d50f9d2727e7aa57f6a9ad88fab6
Nick Hella <NickHella@macxbook.local>
2020-02-06 00:41:23 -0500

    Cleaning up some files and adding blueprint outline of final program, called IMDB_Query_Engine.py

---
+++ IMDB_Query_Engine.py
@@ -0,0 +1,86 @@
+from csvtodb import CSV2DB
+from Query_Interpreter import Query_Interpreter
+
+class IMDB_Query_Engine:
+
+
+	def run(self):
+
+
+		# Intro line
+		print("\n Welcome to the IMDB data set query service brought to you by the Pandas! \n")
+
+		# Load the IMDB database
+		print("Loading CSVs into Sqlite DB...")
+		CSV2DB.csvtodb(self)
+		print("Done!")
+
+		# Provide example of query structure, taken from Query_Syntax.txt
+		query_structure = '''
+
+Query example:
+
+	title,date,genre title "Harry Potter" year "2008"
+	                                  ^ Value we're going to look for
+	                            ^ Column were going to query on
+	                ^ Value we're going to look for
+	       ^ Column were going to query on
+	^ Desired data field we want to return       
+
+
+	This would return:
+	  Harry Potter and the Half-Blood Prince 
+
+	type 'man' for user manual.
+
+'''
+
+		# Print the example query syntax
+		print(query_structure)
+
+
+		# While-True loop for continuous run with designated exit parameter
+		while True:
+
+			# First, prompt the user to type their query:
+			print("\n Please enter your query: \n")
+
+			# Gathering the user's input and defining variable query
+			query = input()
+
+			# Exit parameter
+			if str(query).strip() == 'exit':
+				self.exit()
+			if str(query).strip() == 'help':
+				self.help()
+			if str(query).strip() == 'man':
+				self.help()
+			else:
+
+				# Check to make sure that the query passes data field validation testing
+				@@Exception_Handler
+				Query_Interpreter.data_field_check(self,query)	# checking for proper data fields in the query
+
+				# Check to make sure that the query passes data value validation testing 
+				# @Exception_Handler
+				# Query_Interpreter.data_value_check(query)
+
+				# Run the query through SQLite3 Translation Function
+
+
+
+
+				# Print the result set
+
+
+
+		#
+
+
+	def Exception_Handler():
+
+		print("Exception Handler function called")
+
+
+self = IMDB_Query_Engine()
+self.run()
--- csvtodb.py
+++ csvtodb.py
@@ -6,180 +6,190 @@ import os
 import sys
 import ast
 
+class CSV2DB:
+    '''
+
+    Challenges: single quotes in data, and data encoding.
 
 
-'''
+    '''
 
-Challenges: single quotes in data, and data encoding.
+    # Debug variable for running function stand-alone
+    debug = 1
 
+    def csvtodb(self):
+        
+        # get list of csv files in directory
+        extension = 'csv'
+        result = glob.glob('*.{}'.format(extension))
+        #print(result)
 
-'''
+        # remove any previously existing db file
 
-# Debug variable for running function stand-alone
-debug = 1
-
-def csvtodb():
-    
-    # get list of csv files in directory
-    extension = 'csv'
-    result = glob.glob('*.{}'.format(extension))
-    #print(result)
-
-    # remove any previously existing db file
-
-    if debug == 1:
         try:
-            os.remove("database.db")
+            os.remove("imdb.db")
         except:
             pass
 
 
-    # create the db file
-    conn = sqlite3.connect("database.db")
-    cur = conn.cursor()
+        # create the db file
+        conn = sqlite3.connect("imdb.db")
+        cur = conn.cursor()
 
 
-# Creating the tables (no inserts yet)
+    # Creating the tables (no inserts yet)
 
-    # iterate through csv list
-    for csvfile in result:
+        # iterate through csv list
+        for csvfile in result:
 
-        table_name = csvfile[:-4]
+            table_name = csvfile[:-4]
 
-        sql_string = "CREATE TABLE IF NOT EXISTS " + str(table_name).strip() + "("
-
-
-        try:
-            df = pd.read_csv(csvfile, header=0, index_col=False, encoding="ISO-8859-1")
-        except Exception as e:
-            print("error:")
-            print(tmp_string)
-            print(e)
-            sys.exit()
-        csv_columns = df.columns.tolist()
-
-        if debug:
-            print(csv_columns)
+            sql_string = "CREATE TABLE IF NOT EXISTS " + str(table_name).strip() + "("
 
 
-        for column in csv_columns:
+            try:
+                df = pd.read_csv(csvfile, header=0, index_col=False, encoding="ISO-8859-1")
+            except Exception as e:
+                print("error:")
+                print(tmp_string)
+                print(e)
+                sys.exit()
+            csv_columns = df.columns.tolist()
 
-            sql_string += str(column).strip() + " Text,"
+            if CSV2DB.debug:
+                print(csv_columns)
 
-        # getting rid of the last character in the string (the comma)
-        sql_string = sql_string[:-1]
 
-        # adding the rest of the string
-        sql_string += ");"
+            for column in csv_columns:
+
+                sql_string += str(column).strip() + " Text,"
+
+            # getting rid of the last character in the string (the comma)
+            sql_string = sql_string[:-1]
+
+            # adding the rest of the string
+            sql_string += ");"
+            
+            #print(sql_string)
+
+            cur.execute(sql_string)
+
+    # Insert into tables
         
-        #print(sql_string)
+        # iterating through the tables
+        for csvfile in result:
 
-        cur.execute(sql_string)
+            # Defining the table name as the CSV file without the .csv extension
+            table_name = csvfile[:-4]
 
-# Insert into tables
-    
-    # iterating through the tables
-    for csvfile in result:
+            # get the column names of the headers
+            df = pd.read_csv(str(csvfile), header=0, index_col=False, encoding="ISO-8859-1")
+            csv_columns = df.columns.tolist()
 
-        # Defining the table name as the CSV file without the .csv extension
-        table_name = csvfile[:-4]
+            # print("\n CSV File::")
+            # print(csvfile)
 
-        # get the column names of the headers
-        df = pd.read_csv(str(csvfile), header=0, index_col=False, encoding="ISO-8859-1")
-        csv_columns = df.columns.tolist()
-
-        print("\n CSV File::")
-        print(csvfile)
-
-        print("Column headers in CSV file: ")
-        print(csv_columns)
+            # print("Column headers in CSV file: ")
+            # print(csv_columns)
 
 
-        # let's initialize an empty dictionary
-        tmp_dict = dict(())
-        for column in csv_columns:
-            tmp_dict[column] = None
+            # let's initialize an empty dictionary
+            tmp_dict = dict(())
+            for column in csv_columns:
+                tmp_dict[column] = None
 
 
-        #iterate through the csv file row-by-row to insert data
-        for index, rows in df.iterrows(): 
+            #iterate through the csv file row-by-row to insert data
+            for index, rows in df.iterrows(): 
 
-                # creating row value
-                row = rows.tolist()
+                    # creating row value
+                    row = rows.tolist()
 
-                # populating the dictionary
-                counter2 = 0
-                for column in csv_columns:
-                    tmp_dict[column] = row[counter2]
-                    counter2 += 1
+                    # populating the dictionary
+                    counter2 = 0
+                    for column in csv_columns:
+                        tmp_dict[column] = row[counter2]
+                        counter2 += 1
 
-                # create base insert string
-                base_sql_insert_string = "INSERT INTO " + str(table_name) + "("
+                    # create base insert string
+                    base_sql_insert_string = "INSERT INTO " + str(table_name) + "("
 
-                for column in csv_columns:
-                    base_sql_insert_string += "'" + str(column) + "',"
+                    for column in csv_columns:
+                        base_sql_insert_string += "'" + str(column) + "',"
 
-                # get rid of the last comma
+                    # get rid of the last comma
 
-                base_sql_insert_string = base_sql_insert_string[:-1]
+                    base_sql_insert_string = base_sql_insert_string[:-1]
 
-                base_sql_insert_string += ") VALUES ("
+                    base_sql_insert_string += ") VALUES ("
 
-                for key, value in tmp_dict.items():
+                    for key, value in tmp_dict.items():
 
-                    value = str(value).replace("'", "''")
+                        value = str(value).replace("'", "''")
 
-                    base_sql_insert_string += "'" + str(value) + "',"
+                        base_sql_insert_string += "'" + str(value) + "',"
 
-                # remove the last comma
+                    # remove the last comma
 
-                base_sql_insert_string = base_sql_insert_string[:-1]
+                    base_sql_insert_string = base_sql_insert_string[:-1]
 
-                base_sql_insert_string += ");"
+                    base_sql_insert_string += ");"
 
-                sql_insert_string = base_sql_insert_string
+                    sql_insert_string = base_sql_insert_string
 
-                try:
-                    cur.execute(sql_insert_string)
-                except Exception as e:
-                    print(sql_insert_string)
-                    print(e)
-                    return
-
-
-    if debug == 1:
-
-        print("\n\n Names of tables in the db: ")
-
-        # Let's see the tables that are available in our database..
-        sql = "SELECT name as 'Tables' FROM sqlite_master WHERE type ='table' AND name NOT LIKE 'sqlite_%';"
-
-        # Now let's fetch our result set and load it into a pandas dataframe
-        df = pd.read_sql_query(sql, conn)
-
-        # Printing the results... (the tables)
-        print(df)
-
-        tables = df['Tables'].tolist()
+                    try:
+                        cur.execute(sql_insert_string)
+                    except Exception as e:
+                        print(sql_insert_string)
+                        print(e)
+                        return
 
 
-        for table in tables:
+        if CSV2DB.debug == 1:
 
-            sql = "SELECT * FROM " + str(table) + ";"
+            print("\n\n Names of tables in the db: ")
+
+            # Let's see the tables that are available in our database..
+            sql = "SELECT name as 'Tables' FROM sqlite_master WHERE type ='table' AND name NOT LIKE 'sqlite_%';"
 
             # Now let's fetch our result set and load it into a pandas dataframe
             df = pd.read_sql_query(sql, conn)
 
-            print("Length of table: " + str(table) + " is:")
-
             # Printing the results... (the tables)
-            print(len(df))
+            print(df)
+
+            tables = df['Tables'].tolist()
+
+
+            for table in tables:
+
+                sql = "SELECT * FROM " + str(table) + ";"
+
+                # Now let's fetch our result set and load it into a pandas dataframe
+                df = pd.read_sql_query(sql, conn)
+
+                print("Length of table: " + str(table) + " is:")
+
+                # Printing the results... (the tables)
+                print(len(df))
+
+
+            
+            sql = "SELECT title, year, rank, genre FROM movies INNER JOIN movies_genres ON movies.id = movies_genres.id WHERE title LIKE '%Dalmatians%' GROUP BY title ORDER BY title;"
+
+            # Now let's fetch our result set and load it into a pandas dataframe
+            df = pd.read_sql_query(sql, conn)
+
+            # Printing the results...
+            print(df)
 
 
 
-    conn.commit()
-    conn.close()
+        conn.commit()
+        conn.close()
 
-csvtodb()
+
+self = CSV2DB()
+self.csvtodb()
 
 #EOF
